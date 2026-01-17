2781f71c92d250f8a435b63f0c1c7cd28eb0a6a4
adnankhan-eng <adnankhan48978@gmail.com>
2026-01-17 17:11:37 +0500

    git undo

---
+++ app.js
@@ -0,0 +1,18 @@
+// app.js
+const http = require('http');
+
+// Create a basic web server
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, this is my first Node.js app!\n');
+});
+
+// Server listens on port 3000
+server.listen(3000, () => {
+  console.log('Server is running at http://localhost:3000');
+  
+  <h2>ghjyuio</h2>
+  <h2>ghjyuio</h2>
+  <h2>ghjyuio</h2>
+  <h2>ghjyuio</h2>
+});
